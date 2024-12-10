72cc27e95b485f4de2363b5f9bdd9b6b55801a1e
Tony Huynh <tonyduy092@gmail.com>
2024-12-10 11:12:06 -0800

    add game fps limit

--- main.py
+++ main.py
@@ -9,6 +9,10 @@ def main():
     print(f"success = {numpass}, failed = {numfail}") # debug msg
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    # todo: fps
+    clock = pygame.time.Clock()
+    dt = 0  # delta time
+
     # Game loop
     while True:
         for event in pygame.event.get():
@@ -17,6 +21,9 @@ def main():
 
         screen.fill(Color('black'))
         pygame.display.flip()   # update the display
+
+        # restrict frame rate to 60 FPS
+        dt = clock.tick(GAME_FPS) / 1000    # convert from ms to sec
 
 
 if __name__ == "__main__":
